b64966032347b823a0d9f64f115d861ca3fd4867
Wojciech <womielczarek@gmail.com>
2021-01-26 18:21:09 +0100

    OurWork page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import GlobalStyle from './components/GlobalStyle';
 //Page components
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+//Router
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
+      <Switch> //stops rendering as soon as first address matches
+      //exact means to render AboutUs when url is exact "/" and not for example "/work", because by default react will stop rendering as soon as it finds "/" even if we want /work
+      <Route path="/" exact> 
+        <AboutUs/>
+      </Route>
+      <Route path="/work">
+        <OurWork/>
+      </Route>
+      <Route path="/contact">
+        <ContactUs/>
+      </Route>
+      </Switch>
     </div>
   );
 };
