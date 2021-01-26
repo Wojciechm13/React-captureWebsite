117c553dc94e03eef0a8ec7681ec2a9039acb5cb
Wojciech <womielczarek@gmail.com>
2021-01-26 20:36:55 +0100

    8. Framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import home1 from '../img/home1.png';
-//Styled
-import styled from 'styled-components';
+
 import {SAbout, SDescription, SImage, SHide} from '../styles';
+//Framer motion
+import {motion} from 'framer-motion';
 
 const AboutSection = () =>{
+
+    const titleAnim = {
+        hidden: {opacity: 0},
+        show: {opacity:1, transition: {duration:2}},
+    };
+    const container = {
+        hidden: {x: 100},
+        show: {x: 0, transition: {duration:0.55, staggerChildren: 0.5}},
+    };
+
+
     return(
         <SAbout>
             <SDescription>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <SHide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </SHide>
                     <SHide>
-                        <h2>Your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>Your <span>dreams</span> come</motion.h2>
                     </SHide>
                     <SHide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </SHide>
-                </div>
+                </motion.div>
                 <p>Contact us for any phototgraphy or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
             </SDescription> 
