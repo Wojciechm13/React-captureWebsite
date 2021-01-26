68c77ff106431fc3dd813c41a324e053befc63a0
Wojciech <womielczarek@gmail.com>
2021-01-26 12:36:37 +0100

    5.StyledComponents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+//Global style
+import GlobalStyle from './components/GlobalStyle';
 //Page components
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,62 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+//Styled
+import styled from 'styled-components';
 
 const AboutSection = () =>{
     return(
-        <div>
-            <div className="description">
+        <SAbout>
+            <SDescription>
                 <div className="title">
-                    <div className="hide">
+                    <SHide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>Your <span>dreams</span>come</h2>
-                    </div>
-                    <div className="hide">
+                    </SHide>
+                    <SHide>
+                        <h2>Your <span>dreams</span> come</h2>
+                    </SHide>
+                    <SHide>
                         <h2>true.</h2>
-                    </div>
+                    </SHide>
                 </div>
                 <p>Contact us for any phototgraphy or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
-            </div> 
-            <div className="image">
+            </SDescription> 
+            <SImage>
                 <img src={home1} alt="guy with a camera"/>
-            </div>
-        </div>
+            </SImage>
+        </SAbout>
     );
 };
+
+//Styled components
+
+const SAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const SDescription = styled.div`
+    flex:1;
+    padding-right:5rem;
+    font-weight:lighter;
+`;
+
+const SImage = styled.div`
+    flex:1;
+    overflow:hidden;
+    img{
+        width:100%;
+        height:80vh;
+        object-fit:cover;
+    }
+`;
+
+const SHide = styled.div`
+    overflow:hidden;
+`;
 
 export default AboutSection;
